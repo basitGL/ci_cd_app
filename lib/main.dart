3ede971faa4279ffd74ac4fbe83759a205c03275
basitGL <152172782+basitGL@users.noreply.github.com>
2024-10-11 12:35:34 +0500

    test case added

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          ),
           useMaterial3: true,
         ),
         home: const MyHomePage(title: 'Home Page'),
